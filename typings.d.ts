360dc6a754ae1b38e0fa7a8ba320dcc0c856caaf
Thomas <nyumat18@gmail.com>
2023-01-10 03:14:47 -0800

    [Pole] Finished Initial Type Definition File

---
+++ typings.d.ts
@@ -0,0 +1,62 @@
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+export interface Title {
+  title: string;
+  query: string;
+}
+
+export interface Movie {
+  [x: string]: any;
+  results?: Movie[];
+  title: string;
+  backdrop_path: string;
+  media_type?: string;
+  release_date?: string;
+  first_air_date: string;
+  genre_ids: number[];
+  id: number;
+  name: string;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  vote_average: number;
+  vote_count: number;
+  adult: boolean;
+  video: boolean;
+  original_title: string;
+}
+
+export interface TvShow {
+  backdrop_path: string;
+  first_air_date: string;
+  genre_ids: number[];
+  id: number;
+  name: string;
+  origin_country: string[];
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  vote_average: number;
+  vote_count: number;
+  original_language: string;
+  original_name: string;
+}
+
+export interface TmdbResponse {
+  [x: string]: any;
+  page?: number;
+  results?: Movie[] | TvShow[]
+  total_pages?: number;
+  total_results?: number;
+}
+
+export interface LayoutProps {
+  children: React.ReactNode;
+  isPathRoot: boolean;
+}
