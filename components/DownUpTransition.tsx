4350244fcf9f3f648c3bf0a19428e82cf96b522f
Thomas <nyumat18@gmail.com>
2023-01-10 03:03:58 -0800

    [Feature] Full Page Transition Wrappers w/ Framer Motion

---
+++ components/DownUpTransition.tsx
@@ -0,0 +1,38 @@
+import { motion, AnimatePresence } from 'framer-motion';
+import { useRouter } from 'next/router';
+
+interface TransitionProps {
+  children: React.ReactNode;
+}
+
+const variants = {
+  out: {
+    opacity: 0,
+    y: 40,
+    transition: {
+      duration: 1.0,
+    },
+  },
+  in: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.75,
+      delay: 1.75,
+    },
+  },
+};
+
+export default function Transition({ children }: TransitionProps) {
+  const { asPath } = useRouter();
+
+  return (
+    <div className='effect-1'>
+      <AnimatePresence initial={false} mode='popLayout'>
+        <motion.div key={asPath} variants={variants} animate='in' initial='out' exit='out'>
+          {children}
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
+}
---
+++ components/PopOutTransition.tsx
@@ -0,0 +1,69 @@
+import { motion, AnimatePresence, useReducedMotion } from 'framer-motion';
+import { useRouter } from 'next/router';
+
+interface TransitionProps {
+  children: React.ReactNode;
+}
+
+const variants = {
+  in: {
+    scale: 0.8,
+    y: 100,
+    x: '100%',
+    transition: {
+      duration: 0.4,
+    },
+  },
+  center: {
+    x: 0,
+    scale: 0.8,
+    transformOrigin: 'top',
+    transition: {
+      duration: 0.4,
+    },
+  },
+  scaleUp: {
+    scale: 1,
+    y: 0,
+    transition: {
+      duration: 0.4,
+      delay: 0.5,
+    },
+  },
+  scaleDown: {
+    scale: 0.8,
+    y: 100,
+    transition: {
+      duration: 0.4,
+    },
+  },
+  out: {
+    opacity: 0,
+    x: '-100%',
+    transition: {
+      duration: 0.4,
+      delay: 0.5,
+    },
+  },
+};
+
+export default function PopOutTransition({ children }: TransitionProps) {
+  const { asPath } = useRouter();
+  const shouldReduceMotion = useReducedMotion();
+
+  return (
+    <div className='effect-3'>
+      <AnimatePresence initial={false} mode='wait'>
+        <motion.div
+          key={asPath}
+          variants={!shouldReduceMotion ? variants : undefined}
+          initial='in'
+          animate={['center', 'scaleUp']}
+          exit={['scaleDown', 'out']}
+        >
+          {children}
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
+}
