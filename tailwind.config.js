6132ac939f409c07696b95f71006324bf4267726
Thomas <nyumat18@gmail.com>
2023-01-10 02:38:19 -0800

    Added color palette and styling globals

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,38 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      'xxs': '280px',
+      'xs': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      backgroundImage: {
+        'movie-banner': "url('/movie-banner.jpg')",
+      },
+      colors: {
+        transparent: 'transparent',
+        'shark': {
+          '50': '#f4f6f7',
+          '100': '#e3e7ea',
+          '200': '#cad0d7',
+          '300': '#a5b0bb',
+          '400': '#788698',
+          '500': '#5d6a7d',
+          '600': '#505a6a',
+          '700': '#454c59',
+          '800': '#3e434c',
+          '900': '#2e3138',
+        },
+        'white': '#ffffff',
+        'black': '#000000',
+
+      },
+    },
   },
   plugins: [],
 }
