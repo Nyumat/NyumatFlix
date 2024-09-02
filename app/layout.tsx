061dd7e790547b0ff2a2215d15fe5c4ba8a837ad
Nyuma <tom.nyuma@makenotion.com>
2024-09-01 22:15:36 -0400

    💡 feat: use next-extras to get full path

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,30 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
-import { Navbar } from "@/components/layout/navbar";
+import { cn, isBrowser } from "@/lib/utils";
+import { MarketingNavbar, Navbar } from "@/components/layout/navbar";
 import { ThemeProvider } from "@/components/layout/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
+import { headers } from 'next/headers';
+import { pathname } from 'next-extra/pathname';
 
 export const metadata: Metadata = {
-  title: "Shadcn - Landing template",
-  description: "Landing template from Shadcn",
+  title: "NyumatFlix - Watch Movies and TV Shows Online",
+  description: "Tired of Netflix? Try NyumatFlix! Watch movies and TV shows online for free.",
+  openGraph: {
+    images: [
+      {
+        url: "https://nyumatflix.com/preivew.jpg",
+        width: 1200,
+        height: 630,
+        alt: "NyumatFlix - Watch Movies and TV Shows Online",
+      }
+    ],
+    tags: ["movies", "tv shows", "streaming", "watch online", "free"],
+    description: "Tired of Netflix? Try NyumatFlix! Watch movies and TV shows online for free.",
+    siteName: "NyumatFlix",
+    title: "NyumatFlix - Watch Movies and TV Shows Online",
+  }
 };
 
 export default function RootLayout({
@@ -16,6 +32,9 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const base = `${headers().get('x-forwarded-proto')}://${headers().get('host')}`;
+  const fullUrl = new URL(pathname(), base);
+  const isMarketing = fullUrl.pathname === "/";
   return (
     <html lang="pt-br" suppressHydrationWarning>
       <body className={cn("min-h-screen bg-background", inter.className)}>
@@ -25,7 +44,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar/>
+          {isMarketing ? <MarketingNavbar /> : <Navbar />}
 
           {children}
         </ThemeProvider>
