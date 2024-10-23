67d3f6e4251020246044fc4344d1d046ad6d1468
Thomas <nyumat18@gmail.com>
2024-10-23 12:54:57 -0700

    🍦 seo: add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,7 +25,24 @@ const MyApp = (props: AppProps) => {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
         <title>NyumatFlix - Streaming, Streamlined.</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
         <meta name="theme-color" content="#000000" />
         <meta
           name="description"
