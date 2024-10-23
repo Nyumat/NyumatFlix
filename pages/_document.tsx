082e13b650b8c6052d6e8a36173d6305971c4af7
Thomas <nyumat18@gmail.com>
2024-10-23 13:08:35 -0700

    🚀 append vercel analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import { createGetInitialProps } from "@mantine/next";
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import { Analytics } from "@vercel/analytics/react";
 
 const getInitialProps = createGetInitialProps();
 
@@ -20,6 +21,7 @@ export default class _Document extends Document {
         <body>
           <Main />
           <NextScript />
+          <Analytics />
         </body>
       </Html>
     );
