f76321a31fc87df3ea9d265eb5bd3e807bb99135
Thomas <nyumat18@gmail.com>
2024-10-23 13:30:18 -0700

    👀 seo: add more meta tags for twit and og

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import { MantineProvider } from "@mantine/core";
+import "@styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import Layout from "../components/Layout";
-import "@styles/globals.css";
 
 const MyApp = (props: AppProps) => {
   const { Component, pageProps } = props;
@@ -25,30 +26,40 @@ const MyApp = (props: AppProps) => {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
         <title>NyumatFlix - Streaming, Streamlined.</title>
-        <link rel="shortcut icon" href="/images/favicon.ico" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/images/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/images/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/images/favicon-16x16.png"
-        />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="theme-color" content="#000000" />
         <meta
           name="description"
-          content="NyumatFlix - Streaming, Streamlined."
+          content="The best streaming service for all your needs."
         />
         <meta property="og:image" content="/preview.jpg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta
+          property="og:title"
+          content="NyumatFlix - Streaming, Streamlined."
+        />
+        <meta
+          property="og:description"
+          content="The best streaming service for all your needs."
+        />
+        <meta property="og:url" content="https://nyumatflix.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="NyumatFlix" />
+        <meta property="og:locale" content="en_US" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="NyumatFlix - Streaming, Streamlined."
+        />
+        <meta
+          name="twitter:description"
+          content="The best streaming service for all your needs."
+        />
+        <meta name="twitter:image" content="/preview.jpg" />
+        <meta name="twitter:site" content="https://nyumatflix.com" />
+        <meta name="twitter:creator" content="@notnyumat" />
+        <meta name="twitter:domain" content="nyumatflix.com" />
       </Head>
       <MantineProvider
         withGlobalStyles
@@ -60,6 +71,7 @@ const MyApp = (props: AppProps) => {
         <Layout isPathRoot={isPathRoot}>
           <Component key={router.asPath} {...pageProps} />
         </Layout>
+        <Analytics />
       </MantineProvider>
     </>
   );
