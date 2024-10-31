e33a34ab2bcbfe7e9195103527ae93255752e604
Thomas <nyumat18@gmail.com>
2024-10-31 06:32:49 -0700

    💡 update meta tags with new og image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,7 +32,7 @@ const MyApp = (props: AppProps) => {
           name="description"
           content="The best streaming service for all your needs."
         />
-        <meta property="og:image" content="/preview.jpg" />
+        <meta property="og:image" content="/preview.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta
@@ -56,7 +56,7 @@ const MyApp = (props: AppProps) => {
           name="twitter:description"
           content="The best streaming service for all your needs."
         />
-        <meta name="twitter:image" content="/preview.jpg" />
+        <meta name="twitter:image" content="/preview.png" />
         <meta name="twitter:site" content="https://nyumatflix.com" />
         <meta name="twitter:creator" content="@notnyumat" />
         <meta name="twitter:domain" content="nyumatflix.com" />
