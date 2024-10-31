78a34e3f4235f51a83bddb5c52ae62fbb98decf9
Thomas <nyumat18@gmail.com>
2024-10-31 05:25:33 -0700

    😅 make movies page use new card component

--- pages/movies/[category]/index.tsx
+++ pages/movies/[category]/index.tsx
@@ -171,7 +171,11 @@ export default function CategoryPage({ initialMovies, category }: Props) {
 
           <div className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
             {movies.map((movie: Movie) => (
-              <Card key={`${movie.id}-${movie.title}`} item={movie} />
+              <Card
+                key={`${movie.id}-${movie.title}`}
+                item={movie}
+                mediaType="movie"
+              />
             ))}
           </div>
 
