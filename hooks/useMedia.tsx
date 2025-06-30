6d73b626293127c3e63ae59374fef33638dc03a3
dotcomnerd <Tomxxxfresh@gmail.com>
2025-06-29 20:03:41 -0700

    🖥️ 🖥️ 😎 get intial state depending on server state

--- hooks/useMedia.tsx
+++ hooks/useMedia.tsx
@@ -3,21 +3,12 @@ import { useEffect, useState } from "react";
 
 const getInitialState = (query: string, defaultState?: boolean) => {
   // Prevent a React hydration mismatch when a default value is provided by not defaulting to window.matchMedia(query).matches.
-  if (defaultState !== undefined) {
-    return defaultState;
-  }
-
-  if (isBrowser) {
-    return window.matchMedia(query).matches;
-  }
-
-  // A default value has not been provided, and you are rendering on the server, warn of a possible hydration mismatch when defaulting to false.
-  if (process.env.NODE_ENV !== "production") {
+  if (defaultState !== undefined) return defaultState;
+  if (isBrowser) return window.matchMedia(query).matches;
+  if (process.env.NODE_ENV !== "production")
     console.warn(
-      "`useMedia` When server side rendering, defaultState should be defined to prevent a hydration mismatches.",
+      "`useMedia` When server side rendering, defaultState should be defined to prevent hydration mismatches.",
     );
-  }
-
   return false;
 };
 
