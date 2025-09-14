e393b4935b32637b0bcb1e100c6406c48ded1aaa
dotcomnerd <Tomxxxfresh@gmail.com>
2025-09-14 05:22:12 -0700

    🐛 in route handler `Request` object must b first param

--- app/api/movies/[id]/route.ts
+++ app/api/movies/[id]/route.ts
@@ -1,7 +1,10 @@
-import { movieDb } from "@/lib/constants";
 import { NextResponse } from "next/server";
+import { movieDb } from "@/lib/constants";
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
   const id = params.id;
 
   if (!id) {
