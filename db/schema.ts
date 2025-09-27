305d9549493bc223147b789284e73361dcb3737b
dotcomnerd <Tomxxxfresh@gmail.com>
2025-09-27 12:26:18 -0700

    👀 add prod check for logging connection string

--- db/schema.ts
+++ db/schema.ts
@@ -18,9 +18,11 @@ const connectionString =
 if (!connectionString) {
   throw new Error("DATABASE_URL is not set");
 } else {
-  console.log("========================");
-  console.log(connectionString);
-  console.log("========================");
+  if (process.env.NODE_ENV !== "production") {
+    console.log("========================");
+    console.log(connectionString);
+    console.log("========================");
+  }
 }
 
 const pool = postgres(connectionString, { max: 1 });
