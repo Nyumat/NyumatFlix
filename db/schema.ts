18f74300aef1b1d34bdf17b1c72456df2b024123
dotcomnerd <Tomxxxfresh@gmail.com>
2025-09-27 11:12:50 -0700

    💀 database url somehow not set

--- db/schema.ts
+++ db/schema.ts
@@ -12,8 +12,8 @@ import postgres from "postgres";
 
 const connectionString =
   process.env.NODE_ENV === "production"
-    ? process.env.PROD_DATABASE_URL
-    : process.env.DATABASE_URL;
+    ? process.env.PROD_DATABASE_URL!
+    : process.env.DATABASE_URL!;
 
 if (!connectionString) {
   throw new Error("DATABASE_URL is not set");
