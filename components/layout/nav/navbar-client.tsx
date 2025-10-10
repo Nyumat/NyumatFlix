787e6cd980b5f56a1c026807de31b0d4a0179b3d
dotcomnerd <Tomxxxfresh@gmail.com>
2025-10-10 12:55:12 -0700

    ❌ remove copycat warning temporarily

--- components/layout/nav/navbar-client.tsx
+++ components/layout/nav/navbar-client.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { CopycatWarning } from "@/components/landing/copycat-warning";
 import { NavbarSearchClient } from "@/components/search/search";
 import { cn } from "@/lib/utils";
 import { Session } from "next-auth";
@@ -27,77 +26,70 @@ const NAV_LINKS: NavLink[] = [
 
 interface NavbarClientProps {
   session: Session | null;
-  pathname: string;
 }
 
-export const NavbarClient = ({ session, pathname }: NavbarClientProps) => {
-  const isHomePage = pathname === "/";
-
+export const NavbarClient = ({ session }: NavbarClientProps) => {
   return (
-    <>
-      {isHomePage && (
-        <div className="hidden md:block">
-          <CopycatWarning />
-        </div>
+    <nav
+      className={cn(
+        "absolute top-0 z-50 w-full",
+        "bg-black/90 md:bg-transparent",
       )}
-      <nav
-        className={cn(
-          "absolute top-0 z-50 w-full",
-          "bg-black/90 md:bg-transparent",
-          isHomePage && "md:top-8",
-        )}
-      >
-        <div className="flex justify-between items-center md:max-w-8xl lg:max-w-9xl mx-auto px-4 sm:px-6 lg:px-8 py-4 md:pb-8">
-          <div className="flex flex-row items-center gap-2">
-            <Link href="/">
-              <Image
-                src="/logo.svg"
-                alt="NyumatFlix Logo"
-                width={150}
-                height={150}
-                className="size-10"
-              />
-            </Link>
-            <Tooltip>
-              <TooltipTrigger>
-                <Badge
+    >
+      <div className="flex justify-between items-center md:max-w-8xl lg:max-w-9xl mx-auto px-4 sm:px-6 lg:px-8 py-4 md:pb-8">
+        <div className="flex flex-row items-center gap-2">
+          <Link href="/">
+            <Image
+              src="/logo.svg"
+              alt="NyumatFlix Logo"
+              width={150}
+              height={150}
+              className="size-10"
+            />
+          </Link>
+          <Tooltip>
+            <TooltipTrigger>
+              <Badge
+                className={cn(
+                  "text-xs",
+                  "bg-primary/10 hover:bg-primary/20 text-primary",
+                )}
+              >
+                <span
                   className={cn(
-                    "text-xs",
-                    "bg-primary/10 hover:bg-primary/20 text-primary",
+                    "animate-pulse rounded-full px-2 py-1 text-primary font-bold",
+                    "text-yellow-500 scale-150",
                   )}
                 >
-                  <span
-                    className={cn(
-                      "animate-pulse rounded-full px-2 py-1 text-primary font-bold",
-                      "text-yellow-500 scale-150",
-                    )}
-                  >
-                    •
-                  </span>
-                  Beta
-                </Badge>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>You're using the NyumatFlix 3.0 Beta</p>
-              </TooltipContent>
-            </Tooltip>
-          </div>
-
-          <div className="hidden md:flex items-center space-x-1 lg:space-x-2">
-            <NavbarLinks links={NAV_LINKS} />
-            <NavbarAuth session={session} />
-          </div>
-
-          <div className="flex md:hidden items-center space-x-2">
-            <NavbarAuth session={session} />
-            <NavbarMobileNavigation links={NAV_LINKS}>
-              <div className="px-2">
-                <NavbarSearchClient />
-              </div>
-            </NavbarMobileNavigation>
-          </div>
+                  •
+                </span>
+                Beta
+              </Badge>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>You're using the NyumatFlix 3.0 Beta</p>
+            </TooltipContent>
+          </Tooltip>
         </div>
-      </nav>
-    </>
+
+        {/* search bar for desktop */}
+        <div className="hidden md:flex flex-1 mx-8">
+          <NavbarSearchClient />
+        </div>
+
+        <div className="hidden md:flex items-center space-x-1 lg:space-x-2 ml-auto">
+          <NavbarLinks links={NAV_LINKS} />
+          <NavbarAuth session={session} />
+        </div>
+
+        <div className="flex md:hidden items-center space-x-2">
+          <NavbarMobileNavigation links={NAV_LINKS}>
+            <div className="px-2">
+              <NavbarSearchClient />
+            </div>
+          </NavbarMobileNavigation>
+        </div>
+      </div>
+    </nav>
   );
 };
--- components/layout/nav/navbar-server.tsx
+++ components/layout/nav/navbar-server.tsx
@@ -1,13 +1,9 @@
 "use server";
 
 import { auth } from "@/auth";
-import { headers } from "next/headers";
 import { NavbarClient } from "./navbar-client";
 
 export const NavbarServer = async () => {
   const session = await auth();
-  const headersList = await headers();
-  const pathname = headersList.get("x-pathname") || "/";
-
-  return <NavbarClient session={session} pathname={pathname} />;
+  return <NavbarClient session={session} />;
 };
