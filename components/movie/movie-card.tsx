1fdd2293b1f6603ae42ce14cf7d5556388cfec93
dotcomnerd <70242273+dotcomnerd@users.noreply.github.com>
2026-03-22 05:44:21 -0700

    🃏 cards hover on desktop, link-only grids

--- components/movie/movie-card.tsx
+++ components/movie/movie-card.tsx
@@ -2,6 +2,7 @@
 
 import { HoverCardPortal } from "@radix-ui/react-hover-card";
 import { pages } from "@/config";
+import useMedia from "@/hooks/useMedia";
 import { hasPosterPath } from "@/lib/media-poster-path";
 import { formatValue } from "@/lib/utils";
 import { type Movie } from "@/tmdb/models";
@@ -16,38 +17,57 @@ import { MediaPreview, MediaRating } from "@/components/media/media-shared";
 import Link from "next/link";
 import React from "react";
 
-export const MovieCard: React.FC<Movie> = (props) => {
+export type MovieCardProps = Movie & {
+  variant?: "default" | "linkOnly";
+};
+
+export const MovieCard: React.FC<MovieCardProps> = (props) => {
+  const { variant = "default", ...movie } = props;
   const { id, poster_path, title, vote_average, vote_count, release_date } =
-    props;
+    movie;
+
+  const isMdUp = useMedia("(min-width: 768px)", false);
+  const enableHoverPreview = variant !== "linkOnly" && isMdUp;
 
   if (!hasPosterPath({ poster_path })) {
     return null;
   }
 
-  return (
-    <HoverCard>
-      <HoverCardTrigger asChild>
-        <Link href={`${pages.movie.root.link}/${id}`} key={id} prefetch={false}>
-          <MediaCard.Root className="group">
-            <MediaPoster image={poster_path} alt={title} />
+  const cardInner = (
+    <MediaCard.Root className="group">
+      <MediaPoster image={poster_path} alt={title} />
 
-            <MediaCard.Content>
-              <MediaRating
-                average={vote_average}
-                count={vote_count}
-                className="mb-2"
-              />
-              <MediaCard.Title>{title}</MediaCard.Title>
-              <MediaCard.Excerpt>
-                {formatValue(release_date, format.year)}
-              </MediaCard.Excerpt>
-            </MediaCard.Content>
-          </MediaCard.Root>
-        </Link>
-      </HoverCardTrigger>
+      <MediaCard.Content>
+        <MediaRating
+          average={vote_average}
+          count={vote_count}
+          className="mb-2"
+          showTooltip={enableHoverPreview}
+        />
+        <MediaCard.Title>{title}</MediaCard.Title>
+        <MediaCard.Excerpt>
+          {formatValue(release_date, format.year)}
+        </MediaCard.Excerpt>
+      </MediaCard.Content>
+    </MediaCard.Root>
+  );
+
+  const link = (
+    <Link href={`${pages.movie.root.link}/${id}`} prefetch={false}>
+      {cardInner}
+    </Link>
+  );
+
+  if (!enableHoverPreview) {
+    return link;
+  }
+
+  return (
+    <HoverCard openDelay={500}>
+      <HoverCardTrigger asChild>{link}</HoverCardTrigger>
       <HoverCardPortal>
-        <HoverCardContent side="right" align="start" className="w-96">
-          <MediaPreview {...props} />
+        <HoverCardContent side="bottom" align="start" className="w-96">
+          <MediaPreview {...movie} />
         </HoverCardContent>
       </HoverCardPortal>
     </HoverCard>
--- components/tv/tv-card.tsx
+++ components/tv/tv-card.tsx
@@ -2,6 +2,7 @@
 
 import { HoverCardPortal } from "@radix-ui/react-hover-card";
 import { pages } from "@/config";
+import useMedia from "@/hooks/useMedia";
 import { hasPosterPath } from "@/lib/media-poster-path";
 import { format } from "@/tmdb/utils";
 import { type TvShow } from "@/tmdb/models";
@@ -16,43 +17,61 @@ import { MediaPreview, MediaRating } from "@/components/media/media-shared";
 import Link from "next/link";
 import React from "react";
 
-export const TvCard: React.FC<TvShow> = (props) => {
+export type TvCardProps = TvShow & {
+  variant?: "default" | "linkOnly";
+};
+
+export const TvCard: React.FC<TvCardProps> = (props) => {
+  const { variant = "default", ...show } = props;
   const { id, poster_path, name, vote_average, vote_count, first_air_date } =
-    props;
+    show;
+
+  const isMdUp = useMedia("(min-width: 768px)", false);
+  const enableHoverPreview = variant !== "linkOnly" && isMdUp;
 
   if (!hasPosterPath({ poster_path })) {
     return null;
   }
 
-  return (
-    <HoverCard>
-      <HoverCardTrigger asChild>
-        <Link
-          href={`${pages.tv.root.link}/${id}`}
-          key={id}
-          className="w-full"
-          prefetch={false}
-        >
-          <MediaCard.Root>
-            <MediaPoster image={poster_path} alt={name} />
+  const cardInner = (
+    <MediaCard.Root>
+      <MediaPoster image={poster_path} alt={name} />
 
-            <MediaCard.Content>
-              <MediaRating
-                average={vote_average}
-                count={vote_count}
-                className="mb-2"
-              />
-              <MediaCard.Title>{name}</MediaCard.Title>
-              <MediaCard.Excerpt>
-                {formatValue(first_air_date, format.year)}
-              </MediaCard.Excerpt>
-            </MediaCard.Content>
-          </MediaCard.Root>
-        </Link>
-      </HoverCardTrigger>
+      <MediaCard.Content>
+        <MediaRating
+          average={vote_average}
+          count={vote_count}
+          className="mb-2"
+          showTooltip={enableHoverPreview}
+        />
+        <MediaCard.Title>{name}</MediaCard.Title>
+        <MediaCard.Excerpt>
+          {formatValue(first_air_date, format.year)}
+        </MediaCard.Excerpt>
+      </MediaCard.Content>
+    </MediaCard.Root>
+  );
+
+  const link = (
+    <Link
+      href={`${pages.tv.root.link}/${id}`}
+      className="w-full"
+      prefetch={false}
+    >
+      {cardInner}
+    </Link>
+  );
+
+  if (!enableHoverPreview) {
+    return link;
+  }
+
+  return (
+    <HoverCard openDelay={500}>
+      <HoverCardTrigger asChild>{link}</HoverCardTrigger>
       <HoverCardPortal>
-        <HoverCardContent side="right" align="start" className="w-96">
-          <MediaPreview {...props} />
+        <HoverCardContent side="bottom" align="start" className="w-96">
+          <MediaPreview {...show} />
         </HoverCardContent>
       </HoverCardPortal>
     </HoverCard>
