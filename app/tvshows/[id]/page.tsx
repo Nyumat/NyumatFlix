43482c4c9b10f8d834f84d5f6dbf1c471560207f
dotcomnerd <70242273+dotcomnerd@users.noreply.github.com>
2026-03-22 00:11:16 -0700

    📟 tv overview page

--- app/tvshows/[id]/page.tsx
+++ app/tvshows/[id]/page.tsx
@@ -1,44 +1,11 @@
-import { TvShowDetailTabPanels } from "@/components/tvshow/tv-show-detail-tab-panels";
-import { getCachedTvShowDetail } from "@/lib/media-detail-cache";
-import { generateMediaMetadata } from "@/utils/media-metadata-helpers";
-import { Metadata } from "next";
-import { notFound } from "next/navigation";
-
-export const dynamicParams = true;
+import { pages } from "@/config";
+import { redirect } from "next/navigation";
 
 type Props = {
   params: Promise<{ id: string }>;
 };
 
-export default async function TvShowDetailPage(props: Props) {
+export default async function TVShowDetailRootPage(props: Props) {
   const { id } = await props.params;
-
-  const details = await getCachedTvShowDetail(id).catch(() => null);
-  if (!details) {
-    notFound();
-  }
-
-  return (
-    <TvShowDetailTabPanels
-      tvId={id}
-      numberOfEpisodes={details.number_of_episodes}
-    />
-  );
-}
-
-export async function generateMetadata(props: Props): Promise<Metadata> {
-  const { id } = await props.params;
-  const tvShow = await getCachedTvShowDetail(id).catch(() => null);
-  const base = await generateMediaMetadata({
-    media: tvShow,
-    mediaType: "tv",
-    mediaId: id,
-  });
-  if (!tvShow || !("name" in tvShow)) return base;
-  const year = tvShow.first_air_date
-    ? new Date(tvShow.first_air_date).getFullYear()
-    : "";
-  const nameWithYear = year ? `${tvShow.name} (${year})` : tvShow.name;
-
-  return { ...base, title: `${nameWithYear} | NyumatFlix` };
+  redirect(`${pages.tv.root.link}/${id}/seasons-episodes`);
 }
