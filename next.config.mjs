2319d6813105de1fae670ccb75782b4349773960
dotcomnerd <70242273+dotcomnerd@users.noreply.github.com>
2026-08-07 04:51:35 -0700

    ⚡ next optimize imports

--- next.config.mjs
+++ next.config.mjs
@@ -50,12 +50,7 @@ const nextConfig = {
       },
     ];
   },
-  transpilePackages: [
-    "gsap",
-    "react-three-fiber",
-    "@react-three/drei",
-    "three",
-  ],
+  transpilePackages: ["gsap", "@react-three/fiber", "@react-three/drei"],
   images: {
     unoptimized: true,
     minimumCacheTTL: 60 * 60 * 24 * 30,
@@ -100,7 +95,36 @@ const nextConfig = {
   },
   experimental: {
     taint: true,
-    browserDebugInfoInTerminal: process.env.NODE_ENV !== "production",
+    browserDebugInfoInTerminal:
+      process.env.NEXT_BROWSER_DEBUG === "1" &&
+      process.env.NODE_ENV !== "production",
+    optimizePackageImports: [
+      "@radix-ui/react-accordion",
+      "@radix-ui/react-alert-dialog",
+      "@radix-ui/react-avatar",
+      "@radix-ui/react-checkbox",
+      "@radix-ui/react-collapsible",
+      "@radix-ui/react-context-menu",
+      "@radix-ui/react-dialog",
+      "@radix-ui/react-dropdown-menu",
+      "@radix-ui/react-hover-card",
+      "@radix-ui/react-label",
+      "@radix-ui/react-menubar",
+      "@radix-ui/react-navigation-menu",
+      "@radix-ui/react-popover",
+      "@radix-ui/react-progress",
+      "@radix-ui/react-radio-group",
+      "@radix-ui/react-scroll-area",
+      "@radix-ui/react-select",
+      "@radix-ui/react-separator",
+      "@radix-ui/react-slider",
+      "@radix-ui/react-switch",
+      "@radix-ui/react-tabs",
+      "@radix-ui/react-toast",
+      "@radix-ui/react-toggle",
+      "@radix-ui/react-toggle-group",
+      "@radix-ui/react-tooltip",
+    ],
   },
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
